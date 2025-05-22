7310aa656c2c971d0a45d210c13cd86f033858d9
ROSTAINGmarius <rostaingmarius@gmail.com>
2025-05-22 11:46:13 +0200

    prend en compte les notes

--- Core/Src/midi_data_process.c
+++ Core/Src/midi_data_process.c
@@ -35,9 +35,38 @@ int process_data(uint8_t* data, uint8_t size) {
 }
 
 GPIO_TypeDef* note_to_port(int note) {
-	return GPIOA;
+	if (note %12 == 11){
+		return GPIOB;
+	}
+	else
+		return GPIOA;
 }
 
 int note_to_pin(int note) {
-	return OUT1_Pin;
+	switch (note){
+		case (note %12 == 0):
+				return GPIO_OUT1;
+		case (note %12 == 1):
+				return GPIO_OUT1;
+		case (note %12 == 2):
+				return GPIO_OUT2;
+		case (note %12 == 3):
+				return GPIO_OUT2;
+		case (note %12 == 4):
+				return GPIO_OUT2;
+		case (note %12 == 5):
+				return GPIO_OUT3;
+		case (note %12 == 6):
+				return GPIO_OUT4;
+		case (note %12 == 7):
+				return GPIO_OUT5;
+		case (note %12 == 8):
+				return GPIO_OUT5;
+		case (note %12 == 9):
+				return GPIO_OUT6;
+		case (note %12 == 10):
+				return GPIO_OUT6;
+		case (note %12 == 11):
+				return GPIO_OUT7;
+	}
 }
